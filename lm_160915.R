4048e2544a964fca431f0b1e2fcb3bb6184e051b
nreigl <nicolas.reigl@gmail.com>
2015-09-23 17:29:57 +0300

    update up until point 2) Duan(1983) approach:

--- lm_160915.R
+++ lm_160915.R
@@ -12,7 +12,7 @@ library(psych)
 library(MASS)
 library(arm)
 library(broom)
-
+  
 
 # set wd
 setwd("~/studium/TUT/econometric (phd) - TES9130/R/econometrics - TES9130/econometrics-TES9130")
@@ -32,20 +32,16 @@ pairs(Males)
 discrete.histogram(Males$SCHOOL)
 hist(Males$SCHOOL)
 
-## or via ggplot2
-m <- ggplot(Males, aes(x=SCHOOL))
-m + geom_histogram(aes(y = ..density..),colour = "darkgreen", fill = "white",binwidth = 1)
-m + stat_function(fun = dnorm, aes(colour = 'Normal'))   
-# m + geom_histogram(colour = "darkgreen", fill = "white",binwidth = 1) + geom_density()
 
+# Exp Wage
 Wage<-Males$WAGE
-Males$ExpWage      <- exp(Wage)
-View(Males)
-
+Males$ExpWage<- exp(Wage)
 
 hist(Males$WAGE)
 hist(Males$ExpWage)
 
+## or via ggplot2
+# Hist. Wage
 w0 = ggplot(Males, aes(x=Wage), geom = "blank") +   
   geom_line(aes(y = ..density.., colour = 'Empirical'), stat = 'density') +  
   stat_function(fun = dnorm, aes(colour = 'Normal')) +                       
@@ -54,7 +50,7 @@ w0 = ggplot(Males, aes(x=Wage), geom = "blank") +
   scale_colour_manual(name = 'Density', values = c('red', 'blue'))
 print(w0)
 
-
+# Hist. Exp Wage
 w1 = ggplot(Males, aes(x=ExpWage), geom = "blank") +   
   geom_line(aes(y = ..density.., colour = 'Empirical'), stat = 'density') +  
   stat_function(fun = dnorm, aes(colour = 'Normal')) +                       
@@ -64,6 +60,7 @@ w1 = ggplot(Males, aes(x=ExpWage), geom = "blank") +
 print(w1)
 
 
+# Hist School
 s0 = ggplot(Males, aes(x=SCHOOL), geom = "blank") +   
   geom_line(aes(y = ..density.., colour = 'Empirical'), stat = 'density') +  
   stat_function(fun = dnorm, aes(colour = 'Normal')) +                       
@@ -71,6 +68,9 @@ s0 = ggplot(Males, aes(x=SCHOOL), geom = "blank") +
   labs(title="Histogram for School") + 
     scale_colour_manual(name = 'Density', values = c('red', 'blue'))
 print(s0)
+## discrete variables influence the distribution function
+## proposed workarounds: 1) transform variables or 2) play with bin_geom 3) use lattice to plot it
+
 
 # estimnate OLS regression 
 names(Males)
@@ -78,13 +78,49 @@ lm1<- lm(WAGE ~ EXPER + SCHOOL + UNION + MAR + BLACK, data=Males)
 summary (lm1)
 ## output is correct
 
-# extract coefficients
-lm1.tidy<-tidy(lm1)
-lm1.coef<-lm1.tidy$estimate
-
 # For precise effect size use exp(a)~1+a i.e. a=exp(a)-1
 # we replace the parameter estimate with the exponant -1 to get the more exact effect
 # calulate exact the log and the exp function
 # the exponent of the exact calculation should be larger
+lm1.exactlog<-exp(lm1$coefficients)-1
 
+# row bind the approximation and the exact log beta_hat
+beta_hat.comparison<-rbind(lm1$coefficients, lm1.exactlog)
+beta_hat.comparison
 
+# III GENERATE WAGE PREDICTION IN LEVELS - THE RETRANSFORMATION PROBLEM ON LOG-LINEAR MODEL //
+# Note that exp(E(ln y) is not equal to E(y), BUT E(y|x)=exp(x'b)E(exp(u))
+# 1) Assuming the error term, u, is normally distributed u=N(0, sigma^2), then E(y|x)=exp(x'b)exp(0.5 s^2)
+# find exact number in levels (in Dollar) not in log Dollars
+
+# "old way"
+lyhat_mean<-mean(fitted.values(lm1))
+lyhat_sd<-sd(fitted.values(lm1))
+lyhat_min<-min(fitted.values(lm1))
+lyhat_max<-max(fitted.values(lm1))
+cbind(lyhat_mean, lyhat_sd, lyhat_min, lyhat_max)
+
+# dplyr way 
+df<- as.data.frame(predict.lm(lm1)) 
+names(df)[names(df)=="predict.lm(lm1)"] <- "lnyhat" # rename columnnam
+
+lnyhat.df <-df %>%
+  summarise(length(lnyhat),
+            mean = mean(lnyhat), 
+            st.dev. = sd(lnyhat),
+            min = min(lnyhat),
+            max = max (lnyhat))
+            
+
+# assuming normally distributed error terms we can have 0.5. should work over the predict function in R
+rse<-summary(lm1)$sigma # extract residual standard error for simplicity
+yhatnormal<-exp(lnyhat.df$mean)*exp((0.5)*rse^2)
+yhatnormal
+## not exactly the same results as stata!
+
+# 2) Duan(1983) approach: apply a weaker assumption of iid error term and estimate E(exp(u))=avg(exp(u_hat)), then E(y|x)=exp(x'b)avg(exp(u_hat))
+# predict residuals u_ha
+
+############################################
+# Last commit ##############################
+############################################
