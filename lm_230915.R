f329dbdd209978b4ebe87bb891363b4c5ea47594
nreigl <nicolas.reigl@gmail.com>
2015-09-30 14:56:28 +0300

    First commmit

---
+++ lm_230915.R
@@ -0,0 +1,21 @@
+# lm_230915
+
+# load required packages
+library(hadleyverse)
+library(foreign)
+library(haven) # assuming stata13 files
+library(checkpoint)
+library(ProjectTemplate)
+library(explainr)
+library(magrittry
+        library(psych)
+        library(MASS)
+        library(arm)
+        library(broom)
+        
+        
+        # set wd
+        setwd("~/studium/TUT/econometric (phd) - TES9130/R/econometrics - TES9130/econometrics-TES9130")
+
+        
+        
